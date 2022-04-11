33abb0b895dad3a18df87ca91293c07ce9ad0235
Daneric <danericaalbers@hotmail.com>
2022-04-11 12:26:35 +0200

    feat: playing status

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 import discord.ext as commands
+from discord.ext.commands import Bot
+import discord
 
 
 class Dict(dict):
@@ -35,4 +37,12 @@ config = Configuration.load_json("config.json")
 
 print("[+] Loading Selfbot...")
 
-print("[+] Loading Selfbot... [OK]")
+bot = Bot(">>>", self_bot=True)
+
+@bot.event
+async def on_ready():
+    activity = discord.Game(name="Cool", type=3)
+    await bot.change_presence(status=discord.Status.idle, activity=activity)
+    print("[+] Selfbot loaded!")
+
+bot.run(config.token, bot=False)
