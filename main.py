3cab5333ee7513c8bfaa645e1a021894c7f99802
Daneric <danericaalbers@hotmail.com>
2022-04-11 12:45:54 +0200

    feat: btc command

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import json
 import discord.ext as commands
 from discord.ext.commands import Bot
 import discord
-
+from discord_slash.utils.manage_components import ComponentContext, create_actionrow, create_button, create_select, create_select_option
+from discord_slash.model import ButtonStyle
+import requests
 
 class Dict(dict):
     """dot.notation access to dictionary attributes"""
@@ -37,12 +39,21 @@ config = Configuration.load_json("config.json")
 
 print("[+] Loading Selfbot...")
 
-bot = Bot(">>>", self_bot=True)
+bot = Bot(">", self_bot=True)
 
 @bot.event
 async def on_ready():
-    activity = discord.Game(name="Cool", type=3)
+    activity = discord.Game(name="Fortnite Battle Pass", type=3)
     await bot.change_presence(status=discord.Status.idle, activity=activity)
     print("[+] Selfbot loaded!")
+
+@bot.command()
+async def btc(ctx):
+    await ctx.message.delete()
+    r = requests.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR')
+    r = r.json()
+    usd = r['USD']
+    eur = r['EUR']
+    await ctx.send(content="Bitcoin price in USD: $" + str(usd) + "\nBitcoin price in EUR: €" + str(eur), delete_after=10)
 
 bot.run(config.token, bot=False)
